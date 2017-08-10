7c460d1ba6a707f483d66411af3fa05e1a6c2e59
Mattias Geniar <mattias.geniar@gmail.com>
2017-08-10 18:49:49 +0200

    Extend with full CA chains and SNI support (#24)
    * Add support for SNI and retrieving full CA chain
    
    * Fix docblocks
    
    * Style update
    
    * Style update

--- src/Downloader.php
+++ src/Downloader.php
@@ -13,6 +13,12 @@ class Downloader
     /** @var int */
     protected $timeout = 30;
 
+    /** @var bool */
+    protected $sni = true;
+
+    /** @var bool */
+    protected $ca_chain = false;
+
     /**
      * @param int $port
      *
@@ -21,6 +27,30 @@ class Downloader
     public function usingPort(int $port)
     {
         $this->port = $port;
+
+        return $this;
+    }
+
+    /**
+     * @param int $sni
+     *
+     * @return $this
+     */
+    public function usingSni(bool $sni)
+    {
+        $this->sni = $sni;
+
+        return $this;
+    }
+
+    /**
+     * @param int $ca_chain
+     *
+     * @return $this
+     */
+    public function withFullChain(bool $ca_chain)
+    {
+        $this->ca_chain = $ca_chain;
 
         return $this;
     }
@@ -37,14 +67,18 @@ class Downloader
         return $this;
     }
 
-    public function forHost(string $hostName): SslCertificate
+    public function getCertificates(string $hostName): array
     {
         $hostName = (new Url($hostName))->getHostName();
 
+        $ssl_options = [
+            'capture_peer_cert' => true,
+            'capture_peer_cert_chain' => $this->ca_chain,
+            'SNI_enabled' => $this->sni,
+        ];
+
         $streamContext = stream_context_create([
-            'ssl' => [
-                'capture_peer_cert' => true,
-            ],
+            'ssl' => $ssl_options,
         ]);
 
         try {
@@ -74,9 +108,26 @@ class Downloader
 
         $response = stream_context_get_params($client);
 
-        $certificateFields = openssl_x509_parse($response['options']['ssl']['peer_certificate']);
+        $peer_certificate = $response['options']['ssl']['peer_certificate'];
+        $peer_certificate_chain = $response['options']['ssl']['peer_certificate_chain'] ?? [];
+        $certificates = array_merge([$peer_certificate], $peer_certificate_chain);
 
-        return new SslCertificate($certificateFields);
+        $return = [];
+        foreach ($certificates as $certificate) {
+            $certificateFields = openssl_x509_parse($certificate);
+            $return[] = new SslCertificate($certificateFields);
+        }
+
+        return $return;
+    }
+
+    public function forHost(string $hostName): SslCertificate
+    {
+        $hostName = (new Url($hostName))->getHostName();
+
+        $certificates = $this->getCertificates($hostName);
+
+        return $certificates[0] ?? false;
     }
 
     public static function downloadCertificateFromUrl(string $url, int $timeout = 30): SslCertificate
