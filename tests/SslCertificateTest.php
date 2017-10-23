99e256ef6a7862df204e286597b315934caf79ef
Freek Van der Herten <freek@spatie.be>
2017-10-23 11:21:09 +0000

    Apply fixes from StyleCI

--- tests/SslCertificateTest.php
+++ tests/SslCertificateTest.php
@@ -4,8 +4,8 @@ namespace Spatie\SslCertificate\Test;
 
 use Carbon\Carbon;
 use PHPUnit_Framework_TestCase;
-use Spatie\Snapshots\MatchesSnapshots;
 use Spatie\SslCertificate\Downloader;
+use Spatie\Snapshots\MatchesSnapshots;
 use Spatie\SslCertificate\SslCertificate;
 
 class SslCertificateTest extends PHPUnit_Framework_TestCase
