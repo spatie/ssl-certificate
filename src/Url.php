8e53785b69a30686f5ff9d30c799138df0ce3ac7
Mattias Geniar <m@ttias.be>
2018-12-03 21:40:55 +0100

    IDNA functions in PHP are limited to 61 characters, skip the IDNA encoding if the provided URL is longer than that

--- src/Url.php
+++ src/Url.php
@@ -18,8 +18,10 @@ class Url
             $url = "https://{$url}";
         }
 
-        if (function_exists('idn_to_ascii')) {
-            $url = idn_to_ascii($url, false, INTL_IDNA_VARIANT_UTS46);
+        if (strlen($url) < 61) {
+            if (function_exists('idn_to_ascii')) {
+                $url = idn_to_ascii($url, false, INTL_IDNA_VARIANT_UTS46);
+            }
         }
 
         if (! filter_var($url, FILTER_VALIDATE_URL)) {
