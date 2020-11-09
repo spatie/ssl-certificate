9922ff1c93ec460ff065fe3bda317188e5b91788
Ramon Fincken <github@managedwphosting.nl>
2020-11-09 16:10:53 +0100

    [FEATURE] allow set followLocation to be set to zero, ignoring an SSL redirect

--- src/Downloader.php
+++ src/Downloader.php
@@ -33,6 +33,9 @@ class Downloader
 
     /** @var bool */
     protected $verifyPeerName = true;
+    
+    /** @var int */
+    protected $followLocation = 1;
 
     public function usingPort(int $port)
     {
@@ -82,6 +85,14 @@ class Downloader
 
         return $this;
     }
+    
+
+    public function setFollowLocation(int $followLocation)
+    {
+        $this->followLocation = $followLocation;
+
+        return $this;
+    }    
 
     public function fromIpAddress(string $ipAddress)
     {
@@ -152,6 +163,7 @@ class Downloader
             'peer_name' => $hostName,
             'verify_peer' => $this->verifyPeer,
             'verify_peer_name' => $this->verifyPeerName,
+            'follow_location' => $this->followLocation,
         ];
 
         $streamContext = stream_context_create([
