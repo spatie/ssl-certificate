4e694eaab233f4d1e34b9787e27021b34484252e
Chun-Sheng, Li <peter279k@gmail.com>
2020-12-22 15:44:52 +0800

    Improve PHPUnit fixtures and configuration (#138)

--- tests/SslCertificateFromStringTest.php
+++ tests/SslCertificateFromStringTest.php
@@ -14,7 +14,7 @@ class SslCertificateFromStringTest extends TestCase
     /** @var Spatie\SslCertificate\SslCertificate */
     protected $certificate;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
