ddb80f8b67897182a55a0329c98cce83480285fb
Rudie Dirkx <github@hotblocks.nl>
2022-05-04 20:40:06 +0200

    Keep original unknown error message

--- src/Exceptions/CouldNotDownloadCertificate/UnknownError.php
+++ src/Exceptions/CouldNotDownloadCertificate/UnknownError.php
@@ -6,8 +6,16 @@ use Spatie\SslCertificate\Exceptions\CouldNotDownloadCertificate;
 
 class UnknownError extends CouldNotDownloadCertificate
 {
+    protected string $errorMessage;
+
     public function __construct(string $hostName, string $errorMessage)
     {
         parent::__construct("Could not download certificate for host `{$hostName}` because {$errorMessage}");
+
+        $this->errorMessage = $errorMessage;
+    }
+
+    public function getOriginalMessage(): string {
+        return $this->errorMessage;
     }
 }
