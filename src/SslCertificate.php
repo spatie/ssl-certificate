8fadfc9fca32bc5e4b2bb1fb7777455ee40b29b4
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-10-28 15:03:53 +0200

    move der2pem function to SslCertificate class as a static function

--- src/SslCertificate.php
+++ src/SslCertificate.php
@@ -23,7 +23,7 @@ class SslCertificate
     {
         $fileContents = file_get_contents($pathToCertificate);
         if (!str_contains($fileContents,'BEGIN CERTIFICATE')) {
-            $fileContents = der2pem($fileContents);
+            $fileContents = self::der2pem($fileContents);
         }
         return self::createFromString($fileContents);
     }
@@ -50,6 +50,12 @@ class SslCertificate
             $properties['fingerprintSha256'],
             $properties['remoteAddress'],
         );
+    }
+
+    public static function der2pem($der_data): string
+    {
+        $pem = chunk_split(base64_encode($der_data), 64, "\n");
+        return "-----BEGIN CERTIFICATE-----\n".$pem."-----END CERTIFICATE-----\n";
     }
 
     public function __construct(
